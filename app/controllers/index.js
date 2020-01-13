a324b220594abf4f23961e3ac3a2426beb81b801
Kodi Emeka <ashikodi@Kodis-MacBook-Pro.local>
2020-01-13 20:38:25 +0100

    add filter queryparam, set correct date format on filter

---
+++ app/controllers/index.js
@@ -0,0 +1,17 @@
+import Controller from '@ember/controller';
+import { action } from "@ember/object";
+
+export default class IndexController extends Controller {
+  queryParams = ['dateFilter']
+  dateFilter = ''
+
+  @action
+  changeDate(date) {
+    this.set('dateFilter', date[0].toLocaleDateString().replace(/\//g, '-'));
+  }
+
+  @action
+  deleteInvoice(invoice) {
+    invoice.destroyRecord();
+  }
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
-  model() {
-    return this.store.findAll('invoice');
+  queryParams = {
+    dateFilter: {
+      refreshModel: true
+    }
+  }
+
+  model(params) {
+    const filter = {};
+    if (params.dateFilter) {
+      filter.dateSold = params.dateFilter;
+    }
+    return this.store.query('invoice', { filter });
   }
 }
