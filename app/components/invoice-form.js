76be52c14fd537bea2b751932781087ab7375b7a
Kodi Emeka <ashikodi@Kodis-MacBook-Pro.local>
2020-01-13 19:33:22 +0100

    create invoice-form component

---
+++ app/components/invoice-form.js
@@ -0,0 +1,11 @@
+import { action } from "@ember/object";
+
+import Component from '@glimmer/component';
+
+export default class InvoiceFormComponent extends Component {
+  
+  @action
+  addInvoice() {
+
+  }
+}
--- app/routes/add.js
+++ app/routes/add.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class AddRoute extends Route {
+  model() {
+    return {
+      id: 2,
+      title: 'two',
+      amount: '',
+      dateSold: ''
+    }
+  }
 }
