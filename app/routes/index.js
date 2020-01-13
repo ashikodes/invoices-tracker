14fa735f59b43b16b636a83d60c5fa363f7d49ee
Kodi Emeka <ashikodi@Kodis-MacBook-Pro.local>
2020-01-13 19:14:06 +0100

    create add route, add dummy data, display invoice card

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
+  model() {
+    return [
+      { id: 1, title: 'one', amount: 123, dateSold: '2019-11-11' }
+    ]
+  }
 }
